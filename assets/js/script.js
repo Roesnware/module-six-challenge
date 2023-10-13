b1a2a485757bd7818b14580d02b4ccea0292505a
Roesn <Bubwub04@icloud.com>
2023-10-13 11:34:57 -0400

    hooks

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// hooks
+const curr_city_name = document.getElementById("city-name");
+const curr_date = document.getElementById("curr-date");
+const curr_icon = document.getElementById("main-icon");
+const curr_temp = document.getElementById("curr-temp");
+const curr_wind = document.getElementById("curr-wind");
+const curr_humid = document.getElementById("curr-humidity");
+let five_day_forecast_temp = Array.from(document.getElementById("forecast-temp"));
+let five_day_forecast_wind = Array.from(document.getElementById("forecast-wind"));
+let five_day_forecast_humid = Array.from(document.getElementById("forecast-humidity"));
